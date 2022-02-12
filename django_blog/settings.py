78da0fc6bffbd5377ce183ea4e6840d7c26cc2f1
Alemnew Marie <alemnewmarie461@gmail.com>
2022-02-12 09:30:33 +0300

    Using python-dotenv to keep secrets

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -10,17 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1be8v3!!4-&2xv*u6wlg*sblsm(-gea)!3z(*m2h42wd1q_5mw"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
