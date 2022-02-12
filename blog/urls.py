b50e3c80f860943dcd38032ee94324631d15fdbe
Alemnew Marie <alemnewmarie461@gmail.com>
2022-02-12 14:06:03 +0300

    Add URL patterns and templates

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.HomePageView.as_view(), name="post-list"),
+    path("p/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("p/<slug:slug>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("p/<slug:slug>/edit/", views.PostUpdateView.as_view(), name="post-update"),
+    path("p/<slug:slug>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Post
+
+
+class HomePageView(ListView):
+    """Show the home page of the website."""
+
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return Post.objects.filter(status=1)
+
+
+class PostDetailView(DetailView):
+    """Show the detail of a single post."""
+
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/post_detail.html"
+
+
+class PostCreateView(CreateView):
+    """Display post creation form and handle the process."""
+
+    model = Post
+    fields = ["category", "title", "content"]
+    template_name = "blog/post_create.html"
+
+    def form_valid(self, form):
+        # assign the current logged in user as author of the post
+        form.instance.author = self.request.user
+        messages.success(
+            self.request,
+            "You have successfully published a new post.",
+        )
+        return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    pass
+
+
+class PostDeleteView(DeleteView):
+    pass
+
+
+class CategoryView(ListView):
+    pass
