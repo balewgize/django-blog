4de0dec4611bf6cafc064bb818bf8ec36d5636cc
Alemnew Marie <alemnewmarie461@gmail.com>
2022-02-13 11:05:31 +0300

    Post update and delete feature

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -35,19 +36,50 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         # assign the current logged in user as author of the post
         form.instance.author = self.request.user
         if form.instance.status == 0:
-            msg = "Your post has been saved to draft."
+            msg = "Your post has been saved as draft."
         elif form.instance.status == 1:
             msg = "Your post has been published."
         messages.success(self.request, msg)
         return super().form_valid(form)
 
 
-class PostUpdateView(UpdateView):
-    pass
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Display post update form and handle the process."""
+
+    model = Post
+    fields = ["category", "title", "content", "status"]
+    template_name = "blog/post_update.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        if form.instance.status == 0:
+            msg = "Your post has been saved as draft."
+        elif form.instance.status == 1:
+            msg = "Your post has been updated."
+        messages.success(self.request, msg)
+        return super().form_valid(form)
+
+    def test_func(self):
+        # check that the person trying to update the post is owner of the post
+        return self.get_object().author == self.request.user
 
 
-class PostDeleteView(DeleteView):
-    pass
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Display post deletion form and handle the process."""
+
+    model = Post
+    template_name = "blog/post_delete.html"
+
+    def get_success_url(self):
+        # After deleting the post, redirect to user's profile page
+        messages.success(self.request, "Your post has been deleted.")
+        post_slug = self.kwargs["slug"]
+        author = Post.objects.get(slug=post_slug).author
+        return reverse_lazy("accounts:profile", args=(author.slug,))
+
+    def test_func(self):
+        # check that the person trying to delete the post is owner of the post
+        return self.get_object().author == self.request.user
 
 
 class CategoryView(ListView):
