7e1dea6f6ac0857dacb9d9dd36a7320a5fff4372
Alemnew Marie <alemnewmarie461@gmail.com>
2022-02-23 13:50:06 +0300

    Set DEBUG=False for production settings

--- django_blog/settings_prod.py
+++ django_blog/settings_prod.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DJANGO_DEBUG"]
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "tomar-blogs.herokuapp.com",
