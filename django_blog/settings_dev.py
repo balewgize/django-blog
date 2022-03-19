ac0de6aeed11e71ba74042349e05ad9a5eec2dca
Alemnew Marie <alemnewmarie461@gmail.com>
2022-03-19 09:39:36 +0300

    Use local PostgreSQL in development

--- django_blog/settings_dev.py
+++ django_blog/settings_dev.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "PORT": 5432,
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASS"],
     }
 }
 
